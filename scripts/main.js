ea9a64e24e53aca68187e32cde46b8b2f1065ce9
John Hartert <j.blainehartert@gmail.com>
2017-07-25 23:22:18 -0400

    Removes a function to add final functionality. Will redesign everything into tables and provide further design later.

--- scripts/main.js
+++ scripts/main.js
@@ -7,31 +7,38 @@ function getMovieData(searchString){
 }
 //Always return ajax requests promise.
 
-function makeMovieElement(data){
-    var movies = data["Search"];
-    for (var i = 0; i < movies.length; i++){
-        var para = $("<p></p>");
-        var intermed = movies[i]["Title"];
-        para.text(movies[i]["Title"]);
-        $("footer").append(para);
-    }   
+// function makeMovieElement(data){
+//     var movies = data["Search"];
+//     for (var i = 0; i < movies.length; i++){
+//         var para = $("<p></p>");
+//         var intermed = movies[i]["Title"];
+//         para.text(movies[i]["Title"]);
+//         $("footer").append(para);
+//     }   
+// }
+
+function makePosterElement(data){
+    var poster = data["Search"];
+    for (var i = 0; i < poster.length; i++){
+        var x = (poster[i]["Poster"]);
+        var postUp = $("<img>", {"src": x});
+        postUp.text(poster[i]["Poster"]);
+        $("footer").append(postUp);
+    }
 }
+
+
 
 
 //This would essentially serve as my main function.
 $(BUTN_SEL).on("dblclick", function(event){ 
     event.preventDefault();
+    // getMovieData($("#movie-search").val())
+    //     .then(makeMovieElement)
     getMovieData($("#movie-search").val())
-        .then(makeMovieElement)
+        .then(makePosterElement)
 });
 
-//3. Set up makeMovieElement function.
-// Draw the movie information to the DOM.
-
-//4. Set up makePosterElement function.
-// Build an app that presents the user with a search form and queries for poster 
-// information. For each result, use promises to run a function that draws the poster 
-// information to the DOM.
 
 //5. Set up saveMovie function.
 // For each movie in the watch list, draw that movie to 
@@ -53,9 +60,8 @@ $(BUTN_SEL).on("dblclick", function(event){
 //9. Go back through and set everything up with promises and handle errors with .catch.
 
 ///////////////////////////////////Debug/////////////////////////////////////////////////
-//1. Pretty sure it's returning poster information and movie information as the same 
-//thing. Not sure what's going on there.
 
-//2. Whenever you click in the search bar it sends a request, since it's set to on click.
+
+//1. Whenever you click in the search bar it sends a request, since it's set to on click.
 //The information returns false, but I'm not sure if that's correct. Set to dblclick
 //for the time being to avoid multiple searches.
